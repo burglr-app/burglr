96fdb5531afe25916c38734acfcd15c43b390ed4
viniciuspalma <vini_palma@outlook.com>
2016-08-18 02:06:05 -0300

    add references for recipe in ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818021236) do
+ActiveRecord::Schema.define(version: 20160818044857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20160818021236) do
     t.string   "type",       default: ""
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "recipe_id"
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id", using: :btree
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema.define(version: 20160818021236) do
     t.datetime "updated_at",               null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
 end
